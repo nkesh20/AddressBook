1e4a8b906f292132694efbfc781ce6f621bac01c
nkesh20 <nkesh20@freeuni.edu.ge>
2023-01-10 22:45:06 +0400

    Added endpoints, checked them with Postman by passing JSON data

--- app.py
+++ app.py
@@ -1,11 +1,65 @@
 from flask import Flask
+from flask import request
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+@app.post('/registration')
+def sign_up():
+    data = request.get_json()
+    email = data["email"]
+    password = data["password"]
+    first_name = data["first_name"]
+    last_name = data["last_name"]
+    return {email: password, first_name: last_name}
+
+
+@app.get('/login')
+def login():
+    data = request.get_json()
+    email = data["email"]
+    password = data["password"]
+    return {email: password}
+
+
+@app.post('/addAddress')
+def add_address():
+    data = request.get_json()
+    country = data["country"]
+    city = data["city"]
+    street = data["street"]
+    postal_code = data["postcode"]
+    return {"Country": country,
+            "City": city,
+            "Street": street,
+            "Postal Code": postal_code
+            }
+
+
+@app.post('/updateAddress')
+def update_address():
+    data = request.get_json()
+    country = data["country"]
+    city = data["city"]
+    street = data["street"]
+    postal_code = data["postcode"]
+    unique_id = data["id"]
+    return {"Country": country,
+            "City": city,
+            "Street": street,
+            "Postal Code": postal_code,
+            "id": unique_id
+            }
+
+
+@app.delete('/deleteAddress/<unique_id>')
+def delete_address(unique_id: int):
+    return {"id": unique_id}
+
+
+@app.get('/listAddresses/<unique_id>')
+def list_addresses(unique_id: int):
+    return {"id": unique_id}
 
 
 if __name__ == '__main__':
